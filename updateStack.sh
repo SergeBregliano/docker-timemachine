8b7098ed69ee3b9e8027787ffe428e0742b85cf5
Serge <32168282+SergeBregliano@users.noreply.github.com>
2023-11-23 10:23:31 +0100

    Permit docker-compose and docker compose.

--- updateStack.sh
+++ updateStack.sh
@@ -1,25 +1,39 @@
 #!/bin/bash
 
+# Use of docker-compose or docker compose
+DOCKERCOMPOSE=docker-compose
+if !(type docker-compose) ; then
+    DOCKERCOMPOSE='docker compose'
+    echo "Will use docker compose to replace docker-compose command."
+fi
+
 # Update the stack
 echo 'Updating configuration files'
 echo '============================'
 git pull https://github.com/SergeBregliano/docker-timemachine.git main
 echo ''
 
+# Download the new images version
+echo 'Update and build new images'
+echo '==========================='
+$DOCKERCOMPOSE pull
+$DOCKERCOMPOSE build
+echo ''
+
 # Stop last stack, delete it, and start the new one
 echo 'Shutting down servers'
 echo '====================='
-docker compose down
+$DOCKERCOMPOSE down
 echo ''
 
 echo 'Deleting images'
 echo '==============='
-docker compose rm -f
+$DOCKERCOMPOSE rm -f
 echo ''
 
 echo 'Start new images'
 echo '================'
-docker compose up -d --remove-orphans
+$DOCKERCOMPOSE up -d --remove-orphans
 echo ''
 
 echo 'Your stack has been correctly updated.'
